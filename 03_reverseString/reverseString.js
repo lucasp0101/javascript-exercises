d2fd395e437f4563dea262c915e94fa3b1a8b51d
Lucas Piorno <lucasp01012002@gmail.com>
2023-07-11 03:18:52 +0200

    03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(strInput) {
+    if (strInput === '') {
+        return '';
+    }
+    let strOutput = strInput.split('').reverse().join('');
+    return strOutput;
 };
 
 // Do not edit below this line

934824eb0dd86018d951315df3874dbe3e311cf7
Lucas Piorno <lucasp01012002@gmail.com>
2023-07-11 12:58:39 +0200

    07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (typeof temp !== "number")
+    throw new Error("Input must be a number");
+
+  return Math.round((temp - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (typeof temp !== "number")
+    throw new Error("Input must be a number");
+
+  return Math.round((temp * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line

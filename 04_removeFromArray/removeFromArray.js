badbc5bc2994dd112bf8ea58760a8bddef28d030
Lucas Piorno <lucasp01012002@gmail.com>
2023-07-11 03:23:05 +0200

    04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray) {
+    let arrayCopy = originalArray;
+    for(let i = 1; i < arguments.length; i++) {
+        let index = arrayCopy.indexOf(arguments[i]);
+        if (index > -1) {
+            arrayCopy.splice(index, 1);
+        }
+    }
+    return arrayCopy;
 };
 
 // Do not edit below this line

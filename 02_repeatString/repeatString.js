285d28570b2139505a8fd47b2e8f87897cb29ccb
Lucas Piorno <lucasp01012002@gmail.com>
2023-07-11 03:14:44 +0200

    02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(strInput, nTimes) {
+    let strOutput = '';
+    
+    if(nTimes < 0) {
+        return 'ERROR';
+    }
 
+    for(let i = 0; i < nTimes; i++) {
+        strOutput += strInput;
+    }
+    return strOutput;
 };
 
 // Do not edit below this line

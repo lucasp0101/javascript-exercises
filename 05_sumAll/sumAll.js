b4a10025527c16021162aa8337838e0201b39b11
Lucas Piorno <lucasp01012002@gmail.com>
2023-07-11 03:27:13 +0200

    05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    
+    if (num1 > num2)
+        return num1 * (num1 + 1) / 2 - (num2 - 1) * num2 / 2;
+    
+    return num2 * (num2 + 1) / 2 - (num1 - 1) * num1 / 2;
 };
 
 // Do not edit below this line
